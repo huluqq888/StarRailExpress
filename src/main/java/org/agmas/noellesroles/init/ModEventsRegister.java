2ee624187398672dd55427640a2c2e16075752ae
wifi-left <ysy1679682327@outlook.com>
2026-04-12 12:40:55 +0800

    分离小脑

--- src/main/java/org/agmas/noellesroles/init/ModEventsRegister.java
+++ src/main/java/org/agmas/noellesroles/init/ModEventsRegister.java
@@ -874,58 +874,7 @@ public class ModEventsRegister {
         }));
         MapScanner.registerMapScanEvent();
         CustomWinnerClass.registerCustomWinners();
-        OnTeammateKilledTeammate.EVENT.register((victim, killer, isInnocent, deathReason) -> {
-            if (GameUtils.isPlayerAliveAndSurvival(killer)) {
-                if (isInnocent) {
-                    SREGameWorldComponent gameWorldComponent = SREGameWorldComponent.KEY.get(victim.level());
-                    if (gameWorldComponent.isRole(victim, TMMRoles.DISCOVERY_CIVILIAN)) {
-                        // 跳过游客惩罚
-                        return;
-                    }
-                    // 检查是否是疯狂模式下的魔术师，如果是则不算误杀
-                    if (gameWorldComponent.isRole(victim, ModRoles.MAGICIAN)) {
-                        var psychoComponent = SREPlayerPsychoComponent.KEY.get(victim);
-                        if (psychoComponent != null && psychoComponent.getPsychoTicks() > 0) {
-                            // 魔术师处于疯狂模式，不算误杀
-                            return;
-                        }
-                    }
-
-                    if (gameWorldComponent.isRole(victim, ModRoles.VOODOO)) {
-                        return;
-                    }
-                    if (NoellesRolesConfig.HANDLER.instance().accidentalKillPunishment) {
-                        if (deathReason.getPath().equals("revolver_shot")
-                                || deathReason.getPath().equals("sniper_rifle")
-                                || deathReason.getPath().equals("nunchuck_hit")
-                                || deathReason.getPath().equals("bat_hit")
-                                || deathReason.getPath().equals("gun_shot")
-                                || deathReason.getPath().equals("hoan_meirin_attack")
-                                || deathReason.getPath().equals("arrow")
-                                || deathReason.getPath().equals("trident")
-                                || deathReason.getPath().equals("knife_stab")
-                                || deathReason.getPath().equals("knife")
-                                || deathReason.getPath().equals("fell_out_of_train")
-                                || deathReason.getPath().equals("poison")
-                                || deathReason.getPath().equals("throwing_knife_hit")
-                                || deathReason.getPath().equals("bowen")
-                                || deathReason.getPath().equals("fire_axe")) {
-                            GameUtils.killPlayer(killer, true, null, Noellesroles.id("shot_innocent"));
-
-                            // 仇杀客事件：误杀发生时强化仇杀客
-                            for (Player player : victim.level().players()) {
-                                if (gameWorldComponent.isRole(player, ModRoles.BLOOD_FEUDIST)) {
-                                    BloodFeudistPlayerComponent bfComp = ModComponents.BLOOD_FEUDIST.get(player);
-                                    if (bfComp != null) {
-                                        bfComp.onAccidentalKill();
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        XiaoNaoHandler.registerEvent();
         OnPlayerDeathWithKiller.EVENT.register((victim, killer, deathReason) -> {
             SREGameWorldComponent gameWorld = SREGameWorldComponent.KEY.get(victim.level());
             if (gameWorld == null || !gameWorld.isRunning())
---
+++ src/main/java/org/agmas/noellesroles/init/XiaoNaoHandler.java
@@ -0,0 +1,73 @@
+package org.agmas.noellesroles.init;
+
+import org.agmas.noellesroles.Noellesroles;
+import org.agmas.noellesroles.component.BloodFeudistPlayerComponent;
+import org.agmas.noellesroles.component.ModComponents;
+import org.agmas.noellesroles.config.NoellesRolesConfig;
+import org.agmas.noellesroles.role.ModRoles;
+
+import io.wifi.starrailexpress.api.TMMRoles;
+import io.wifi.starrailexpress.cca.SREGameWorldComponent;
+import io.wifi.starrailexpress.cca.SREPlayerPsychoComponent;
+import io.wifi.starrailexpress.event.OnTeammateKilledTeammate;
+import io.wifi.starrailexpress.game.GameUtils;
+import net.minecraft.server.level.ServerPlayer;
+
+public class XiaoNaoHandler {
+
+    public static void registerEvent() {
+        OnTeammateKilledTeammate.EVENT.register((victim, killer, isInnocent, deathReason) -> {
+            if (GameUtils.isPlayerAliveAndSurvival(killer)) {
+                if (isInnocent) {
+                    SREGameWorldComponent gameWorldComponent = SREGameWorldComponent.KEY.get(victim.level());
+                    if (gameWorldComponent.isRole(victim, TMMRoles.DISCOVERY_CIVILIAN)) {
+                        // 跳过游客惩罚
+                        return;
+                    }
+                    // 检查是否是疯狂模式下的魔术师，如果是则不算误杀
+                    if (gameWorldComponent.isRole(victim, ModRoles.MAGICIAN)) {
+                        var psychoComponent = SREPlayerPsychoComponent.KEY.get(victim);
+                        if (psychoComponent != null && psychoComponent.getPsychoTicks() > 0) {
+                            // 魔术师处于疯狂模式，不算误杀
+                            return;
+                        }
+                    }
+
+                    if (gameWorldComponent.isRole(victim, ModRoles.VOODOO)) {
+                        return;
+                    }
+                    if (NoellesRolesConfig.HANDLER.instance().accidentalKillPunishment) {
+                        if (deathReason.getPath().equals("revolver_shot")
+                                || deathReason.getPath().equals("sniper_rifle")
+                                || deathReason.getPath().equals("nunchuck_hit")
+                                || deathReason.getPath().equals("bat_hit")
+                                || deathReason.getPath().equals("gun_shot")
+                                || deathReason.getPath().equals("hoan_meirin_attack")
+                                || deathReason.getPath().equals("arrow")
+                                || deathReason.getPath().equals("trident")
+                                || deathReason.getPath().equals("knife_stab")
+                                || deathReason.getPath().equals("knife")
+                                || deathReason.getPath().equals("fell_out_of_train")
+                                || deathReason.getPath().equals("poison")
+                                || deathReason.getPath().equals("throwing_knife_hit")
+                                || deathReason.getPath().equals("bowen")
+                                || deathReason.getPath().equals("fire_axe")) {
+                            GameUtils.killPlayer(killer, true, null, Noellesroles.id("shot_innocent"));
+
+                            // 仇杀客事件：误杀发生时强化仇杀客
+                            for (ServerPlayer player : victim.serverLevel().players()) {
+                                if (gameWorldComponent.isRole(player, ModRoles.BLOOD_FEUDIST)) {
+                                    BloodFeudistPlayerComponent bfComp = ModComponents.BLOOD_FEUDIST.get(player);
+                                    if (bfComp != null) {
+                                        bfComp.onAccidentalKill();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+}
