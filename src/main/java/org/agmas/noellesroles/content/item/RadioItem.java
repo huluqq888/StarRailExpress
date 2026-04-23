8a966ba9dfefd54d124a11c6578690a15bf84904
wifi-left <ysy1679682327@outlook.com>
2026-04-23 13:49:28 +0800

    删除错误的对讲机代码（你把风精灵的拿来用干啥）

--- src/main/java/org/agmas/noellesroles/content/item/RadioItem.java
+++ src/main/java/org/agmas/noellesroles/content/item/RadioItem.java
@@ -28,15 +28,11 @@ public class RadioItem extends Item {
         }
 
         UUID id = user.getUUID();
-        var comp = PlayerVolumeComponent.KEY.get(user);
         if (RADIO_GROUP.contains(id)) {
             RADIO_GROUP.remove(id);
-            comp.clear();
             user.displayClientMessage(Component.translatable("message.noellesroles.radio.left"), true);
         } else {
             RADIO_GROUP.add(id);
-            // 给出较长时间的 voice volume 状态，具体服务器端语音逻辑可依赖此组件
-            comp.setVolume(20 * 60 * 10, 1.0f);
             user.displayClientMessage(Component.translatable("message.noellesroles.radio.joined"), true);
         }
 
